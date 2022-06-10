5002ff99a3894f08496be727d28ac4c147924202
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-06-10 17:22:41 +0530

    bublesort

--- 16Sorting.java/BubbleSort.java
+++ 16Sorting.java/BubbleSort.java
@@ -1,17 +1,25 @@
 public class BubbleSort {
-    public static void main(String[] args) {
-        int arr[] = {7,8,3,1,2,40};
+    public static void buble(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 
-        //bubble sort
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = 0; j < arr.length-i-1; j++) {
-                if(arr[j]>arr[j+1]){
-                    //swap
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+    public static void main(String[] args) {
+    int arr[] = {12,36,5,8,5,64,4,245,454};
+
+    //time complexity O(n^2)
+    //bubble sort
+
+    for(int i=0;i<arr.length-1;i++){
+        for(int j = 0; j < arr.length-i-1; j++) {
+            if(arr[j] > arr[j+1]){  //
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
+    buble(arr);
+   }
 }
