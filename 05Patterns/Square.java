042b408388ffa734c13c2515ab8afa03898039c7
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-09-18 20:43:15 +0530

    push

---
+++ 05Patterns/Square.java
@@ -0,0 +1,16 @@
+
+
+ public class Square{
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 2;
+
+        for(int i = 1; i<=n; i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+           
+            System.out.println();
+        }
+    }
+ }
