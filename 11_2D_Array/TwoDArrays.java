89cd5f40dd42daf2d7cbbcbe93f3943613bc62df
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-09-19 06:37:57 +0530

    Update TwoDArrays.java

--- 11_2D_Array/TwoDArrays.java
+++ 11_2D_Array/TwoDArrays.java
@@ -1,12 +1,23 @@
 import java.util.*;
 
-public class SumTwoN {
+public class TwoDArrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b;
-        System.out.println(sum);
+        int rows = sc.nextInt();
+        int col =  sc.nextInt();
+        int tarr[][]= new int[rows][col];
 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                tarr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(tarr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
